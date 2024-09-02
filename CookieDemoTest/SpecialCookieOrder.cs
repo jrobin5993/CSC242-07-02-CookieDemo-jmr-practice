3267b8ebb9d73ed9bb83df11ef8f25c19724d66a
jrobin5993 <jrobin5993@my.sullivan.edu>
2024-09-02 11:32:07 -0400

    Step 2: inherit from CookieOrder and override the CalculatePrice method to include special handling fees

---
+++ CookieDemoTest/SpecialCookieOrder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookieDemoTest
+{
+    public class SpecialCookieOrder : CookieOrder
+    {
+        public string SpecialDescription { get; set; }
+
+        protected override void CalculatePrice()
+        {
+            base.CalculatePrice(); // Calculate base price
+
+            if (Price <= 40m)
+                price += 10m; // Special handling fee for orders up to $40
+            else
+                price += 8m;  // Special handling fee for orders over $40
+        }
+    }
+}
