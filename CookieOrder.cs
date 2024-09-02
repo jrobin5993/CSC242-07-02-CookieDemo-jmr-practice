da8de7458ddc1a47cb4a80605ef5cdb766b03862
jrobin5993 <jrobin5993@my.sullivan.edu>
2024-09-02 11:25:16 -0400

    Step 1: Define the CookieOrder Class

---
+++ CookieOrder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class CookieOrder
+{
+    public int OrderNumber { get; set; }
+    public string RecipientName { get; set; }
+    public string CookieType { get; set; }
+    private int dozens;
+    private decimal price;
+
+    public int Dozens
+    {
+        get { return dozens; }
+        set
+        {
+            dozens = value;
+            CalculatePrice();
+        }
+    }
+    public decimal Price
+    {
+        get { return price; }
+    }
+
+    protected virtual void CalculatePrice()
+    {
+        if (dozens <= 2)
+            price = dozens * 15m;
+        else
+            price = (2 * 15m) + ((dozens - 2) * 13m);
+    }
+}
+
